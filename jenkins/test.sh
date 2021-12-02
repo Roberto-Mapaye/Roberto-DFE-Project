df0bc293f5d23ba2b1f450369112abacf0e53521
Ubuntu <rmapaye@dev-jenkins.23nzo2qnrkburp5xzshz5xehbd.parx.internal.cloudapp.net>
2021-12-02 17:00:17 +0000

    Testing v3

--- jenkins/test.sh
+++ jenkins/test.sh
@@ -5,7 +5,7 @@ echo "Test stage"
 python3 -m venv venv
 source venv/bin/activate
 
-pip3 install pytest flask_testing
+pip3 install pytest pytest-cov flask_testing
 pip3 install -r frontend/requirements.txt
 pip3 install -r backend/requirements.txt
 
