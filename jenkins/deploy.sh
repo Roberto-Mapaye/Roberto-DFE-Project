a295260eadd84ffd3001315dfeefe8a83affce80
Ubuntu <rmapaye@dev-jenkins.23nzo2qnrkburp5xzshz5xehbd.parx.internal.cloudapp.net>
2021-12-02 16:05:46 +0000

    Stuff V1

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-echo "Deploy stae"
+echo "Deploy stage"
 
 scp docker-compose.yaml jenkins@swarm-manager-2:/home/jenkins/docker-compose.yaml
 ssh jenkins@swarm-manager-2 \
 DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+CREATE_SCHEMA=true \
+DATABASE_URI=$DATABASE_URI \
 docker stack deploy --compose-file docker-compose.yaml DFE-project
