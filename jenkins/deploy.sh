e23486b54e51d354ebad9eb04eb85ba639aa46bc
Ubuntu <rmapaye@dev-jenkins.23nzo2qnrkburp5xzshz5xehbd.parx.internal.cloudapp.net>
2021-12-02 12:13:04 +0000

    Adding deploy stage v2

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,4 +2,7 @@
 
 echo "Deploy stage"
 
-ssh jenkins@dev-jenkins docker stack deploy --compose-file docker-compose.yaml DFE-project
+scp docker-compose.yaml jenkins@swarm-manager-2:/home/jenkins/docker-compose.yaml
+ssh jenkins@swarm-manager-2 \
+DOCKER_HUB_CREDS_USR=$DOCKER_HUB_CREDS_USR \
+docker stack deploy --compose-file docker-compose.yaml DFE-project
