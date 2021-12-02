cc80b6745e1013f39dc2ce9ae8744bf69d6f33b9
Ubuntu <rmapaye@dev-jenkins.23nzo2qnrkburp5xzshz5xehbd.parx.internal.cloudapp.net>
2021-12-02 15:56:58 +0000

    Testing changes

---
+++ backend/tests/test_unit.py
@@ -0,0 +1,109 @@
+from flask import url_for
+from flask_testing import TestCase
+from application import app, db
+from application.models import Teams, Players
+
+test_team = {
+                "team_id": 1,
+                "team_name": "Back Esports",
+                "game": "CS:GO"
+            }
+
+test_player = {
+                "player_id": 1,
+                "team_id": 1,
+                "first_name": "Back SomeGuy",
+                "last_name": "Back Smith",
+            }
+
+class TestBase(TestCase):
+
+    def create_app(self):
+        # Defines the flask object's configuration for the unit tests
+        app.config.update(
+            SQLALCHEMY_DATABASE_URI='sqlite:///',
+            DEBUG=True,
+            WTF_CSRF_ENABLED=False
+        )
+        return app
+
+    def setUp(self):
+        # Will be called before every test
+        db.create_all()
+        db.session.add(Teams(team_name="Back Esports", game="CS:GO"))
+        db.session.commit()
+
+    def tearDown(self):
+        # Will be called after every test
+        db.session.remove()
+        db.drop_all()
+
+class TestRead(TestBase):
+
+    def test_read_all_teams(self):
+        response = self.client.get(url_for('read_all_teams'))
+        all_teams = { "teams": [test_team] }
+        self.assertEquals(all_teams, response.json)
+    
+    def test_read_team(self):
+        response = self.client.get(url_for('read_team', id=1))
+        self.assertEquals(test_team, response.json)
+    
+    def test_read_all_players(self):
+        response = self.client.get(url_for('read_all_players', id=1))
+        all_players = { "players": [test_player] }
+        self.assertEquals(all_players, response.json)
+    
+    def test_read_player(self):
+        response = self.client.get(url_for('read_players', id=1))
+        self.assertEquals(test_player, response.json)
+
+class TestCreate(TestBase):
+
+    def test_create_team(self):
+        response = self.client.post(
+            url_for('create_team'),
+            json={"team_name": "SLC", "game": "Overwatch"},
+            follow_redirects=True
+        )
+        self.assertEquals(b"Added task with description: Testing create team functionality", response.data)
+        self.assertEquals(Teams.query.get(2).team_name, "SLC")
+
+    def test_create_player(self):
+        response = self.client.post(
+            url_for('create_player', id=1),
+            json={"first_name": "Bruce", "last_name": "Wayne", "team_id":1},
+            follow_redirects=True
+        )
+        self.assertEquals(b"Added task with description: Testing create player functionality", response.data)
+        self.assertEquals(Players.query.get(2).first_name, "Bruce")
+
+class TestUpdate(TestBase):
+
+    def test_update_team(self):
+        response = self.client.put(
+            url_for('update_team', id=1),
+            json={"team_name": "ChangedName Esports"}
+        )
+        self.assertEquals(b"Updated team (ID: 1) with another name: ChangedName Esports", response.data)
+        self.assertEquals(Teams.query.get(1).team_name, "ChangedName Esports")
+
+    def test_update_player(self):
+        response = self.client.put(
+            url_for('update_player', id=1),
+            json={"first_name": "ChangedName Sasha"}
+        )
+        self.assertEquals(b"Updated player (ID: 1) with another first name: ChangedName Sasha", response.data)
+        self.assertEquals(Players.query.get(1).first_name, "ChangedName Sasha")     
+
+class TestDelete(TestBase):
+
+    def test_delete_team(self):
+        response = self.client.delete(url_for('delete_teams', id=1))
+        self.assertEquals(b"Deleted task with ID: 1", response.data)
+        self.assertIsNone(Teams.query.get(1))
+    
+    def test_delete_player(self):
+        response = self.client.delete(url_for('delete_players', id=1))
+        self.assertEquals(b"Deleted task with ID: 1", response.data)
+        self.assertIsNone(Players.query.get(1))
--- jenkins/test.sh
+++ jenkins/test.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
 echo "Test stage"
+
+python3 -m venv venv
+source venv/bin/activate
+
+pip3 install pytest flask_testing
+pip3 install -r frontend/requirements.txt
+pip3 install -r backend/requirements.txt
+
+mkdir test-reports
+
+
+python3 -m pytest frontend --cov=frontend/application \
+    --cov-report term-missing \
+    --cov-report xml:test-reports/frontend_coverage.xml \
+    --junitxml=test-reports/frontend-junit_report.xml
+
+python3 -m pytest backend --cov=backend/application \
+    --cov-report term-missing \
+    --cov-report xml:test-reports/backend_coverage.xml \
+    --junitxml=test-reports/backend-junit_report.xml
+
+deactivate
+rm -rf venv
